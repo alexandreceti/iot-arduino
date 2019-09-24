c514c91c7cd7ae88da47d07f18b95731ebbc4da3
Alexandre C.C.Oliveira <alexandrecunha.eti@gmail.com>
2019-09-23 22:29:14 -0300

    Tirando configuração firebase

--- sketch_firebase/sketch_firebase.ino
+++ sketch_firebase/sketch_firebase.ino
@@ -2,10 +2,12 @@
 #include <FirebaseArduino.h>
 
 // Set these to run example.https://xxxxxx.firebaseio.com/
-#define FIREBASE_HOST "xxxxxx.firebaseio.com"
-#define FIREBASE_AUTH "tokenxxxxxxxx"
-#define WIFI_SSID "wifi"
-#define WIFI_PASSWORD "senha123"
+#define FIREBASE_HOST "xxxxxxxx.firebaseio.com"
+#define FIREBASE_AUTH "tokentokentokentokentoken"
+#define WIFI_SSID "wifi_rede"
+#define WIFI_PASSWORD "Senha do wifi"
+// onde esta o led
+const int pino_led = 16;
 
 void setup() {
   Serial.begin(115200);
@@ -22,19 +24,32 @@ void setup() {
   Serial.println(WiFi.localIP());
   
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  
+  pinMode(pino_led, OUTPUT);
+  int temp = 0;
 }
 void loop() {
-  
+
+  int temp;
   int sensorValue = analogRead(A0);   // Ler o pino Analógico A0 onde está o LDR
   //float voltagem = sensorValue * (3.3 / 1024.0);   // Converter a leitura analógica (que vai de 0 - 1023) para uma voltagem (0 - 3.3V), quanto de acordo com a intensidade de luz no LDR a voltagem diminui.
   float voltagem = map(sensorValue, 0, 1023, 0, 100);
-  Serial.println(voltagem);   // Mostrar valor da voltagem no monitor serial
-  Firebase.setFloat("controle/vol", voltagem); // Envia o dado da variavel voltagem para a variavel LDR no Firebase
-  if (Firebase.failed()) {
-      Serial.print("setting /vol failed:");
-      Serial.println(Firebase.error());  
-      return;
+  
+  if (temp != voltagem) {
+    Serial.println(voltagem);   // Mostrar valor da voltagem no monitor serial
+    Firebase.setFloat("controle/vol", voltagem); // Envia o dado da variavel voltagem para a variavel LDR no Firebase
+    if (Firebase.failed()) {
+        Serial.print("setting /vol failed:");
+        Serial.println(Firebase.error());  
+        return;
+    }
+    digitalWrite(pino_led, HIGH); // acende o LED
+    delay(1000); // aguarda 1 segundo
+    digitalWrite(pino_led, LOW); // apaga o LED
+    delay(1000); // aguarda 1 segundo
   }
+  temp = voltagem;
+ 
   delay(10000); // Espera 5 segundos
   
 }
