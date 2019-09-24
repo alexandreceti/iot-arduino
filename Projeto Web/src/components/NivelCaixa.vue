f2ab1180a120deda69fe9505276a421a32b5aafe
Alexandre C.C.Oliveira <alexandrecunha.eti@gmail.com>
2019-09-23 22:38:43 -0300

    Configurando Readme

---
+++ Projeto Web/src/components/NivelCaixa.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <div class="w3-light-grey">
+      <div
+        class="w3-container w3-padding w3-blue w3-center"
+        :style="nivel(vol)"
+      >{{vol}}%</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NivelCaixa',
+  props: {
+    vol: {
+      type: Number,
+      default: 0
+    }
+  },
+
+  methods: {
+    nivel: function (vol) {
+      return `height:50px; width: ${vol}%`
+    }
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# iot-arduino
+#Trabalho Internet das Coisas: Arquitetura, Tecnologias e Aplicações
+
+#Turma de Arquitetura de Software na Plataforma Java EE
+
+##Arquivo Sketch do arduino
+com nodeMCU e firebase
+
+##Projeto web
+Projeto web Vue com firebase para mostrar o nível.
+
+**PucMinas**
+
+Curso: Arquitetura de Software Distribuído  
+Oferta: 10 – 2019/02  
+Unidade: Praça Liberdade
+
+_Alunos:_
+Alexandre Cunha Cruz Oliveira
+Henrique Franzoni
