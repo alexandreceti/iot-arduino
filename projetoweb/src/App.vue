f6be5f765a91c55108c17c35fc5094ed6651b1ea
Alexandre Oliveira <alexandre@alexandrecunha.eti.br>
2022-02-26 09:08:54 -0300

    teste

---
+++ projetoweb/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style>
+#app {
+}
+#nav {
+}
+
+#nav a {
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
